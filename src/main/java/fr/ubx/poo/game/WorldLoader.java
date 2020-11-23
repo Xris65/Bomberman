12ec0b1ea5f22edc3b30939506c44aaf893b444d
garion.goubard <garion.goubard@etu.u-bordeaux.fr>
2020-11-23 17:53:31 +0100

    fixed x/y problem with map loading and removed debug sout

--- src/main/java/fr/ubx/poo/game/WorldLoader.java
+++ src/main/java/fr/ubx/poo/game/WorldLoader.java
@@ -7,12 +7,11 @@ import java.util.Scanner;
 
 public class WorldLoader {
     public World readFromFile(String filename){
+        // Pretty ugly but needed to work, else "file:" prefix isn't working because of File() not parsing it correctly
         String path = getClass().getResource("/sample/" + filename).toExternalForm().split(":")[1];
         WorldEntity[][] read = null;
         try {
             File myObj = new File(path);
-            System.out.println("File at: " + myObj);
-            System.out.println("Exists: " + myObj.exists() + " readable: " + myObj.canRead());
             Scanner myReader = new Scanner(myObj);
             Scanner measuresReader = new Scanner(myObj);
 
@@ -33,16 +32,13 @@ public class WorldLoader {
             }
             measuresReader.close();
 
-            System.out.println("Map height :" + height);
-            System.out.println("Map width : " + width);
-
             read = new WorldEntity[width][height];
             for(int y = 0; myReader.hasNextLine(); y++){
                 String line = myReader.nextLine();
                 for(int x = 0; x < line.length(); x++){
                     Optional<WorldEntity> readInFileToWorldEntity = WorldEntity.fromCode(line.charAt(x));
                     if(readInFileToWorldEntity.isPresent()){
-                        read[x][y] = readInFileToWorldEntity.get();
+                        read[y][x] = readInFileToWorldEntity.get();
                     } else {
                         throw new RuntimeException(String.format("Character %c is invalid\n", line.charAt(x)));
                     }
