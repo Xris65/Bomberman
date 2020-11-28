8570c43b4c73fdf53e404e07064d284ed79af585
garion <garion@pop-os.localdomain>
2020-11-28 12:43:11 +0100

    Removed WorldLoader because we can just use WorldLoader's function in the WorldManager, which makes more sense

--- src/main/java/fr/ubx/poo/game/WorldManager.java
+++ src/main/java/fr/ubx/poo/game/WorldManager.java
@@ -3,7 +3,11 @@ package fr.ubx.poo.game;
 import fr.ubx.poo.model.go.character.Monster;
 import fr.ubx.poo.model.go.character.Player;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Scanner;
 
 public class WorldManager {
     private ArrayList<World> worlds = new ArrayList<>();
@@ -30,8 +34,7 @@ public class WorldManager {
             return worlds.get(currentWorldIndex);
         } else {
             World nextWorld;
-            WorldLoader wl = new WorldLoader();
-            nextWorld = wl.readFromFile(String.format("level%d.txt", worldMaxAtteint + 1));
+            nextWorld = readFromFile(String.format("level%d.txt", worldMaxAtteint + 1));
             nextWorld.monsters = nextWorld.findMonsters(game);
             addWorld(nextWorld);
             return nextWorld;
@@ -64,6 +67,53 @@ public class WorldManager {
     }
     public int getCurrentWorldIndex() {
         return currentWorldIndex;
+    }
+
+    private World readFromFile(String filename){
+        // Pretty ugly but needed to work, else "file:" prefix isn't working because of File() not parsing it correctly
+        String path = getClass().getResource("/sample/" + filename).toExternalForm().substring("file:".length());
+        WorldEntity[][] read = null;
+        try {
+            File myObj = new File(path);
+            Scanner myReader = new Scanner(myObj);
+            Scanner measuresReader = new Scanner(myObj);
+
+            // Get map height and width
+            int height = 0;
+            int width = -1;
+            for(; measuresReader.hasNextLine(); height++){
+                int tmp = measuresReader.nextLine().length();
+                if(width != -1){
+                    if(tmp != width){
+                        throw new RuntimeException("Map width is not consistent");
+                    }
+                }
+                width = tmp;
+            }
+            if(height == 0){
+                throw new RuntimeException("Can't read empty file");
+            }
+            measuresReader.close();
+
+            read = new WorldEntity[height][width];
+            String line;
+            for(int y = 0; myReader.hasNextLine(); y++){
+                line = myReader.nextLine();
+                for(int x = 0; x < line.length(); x++){
+                    Optional<WorldEntity> readInFileToWorldEntity = WorldEntity.fromCode(line.charAt(x));
+                    if(readInFileToWorldEntity.isPresent()){
+                        read[y][x] = readInFileToWorldEntity.get();
+                    } else {
+                        throw new RuntimeException(String.format("Character %c is invalid\n", line.charAt(x)));
+                    }
+                }
+
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(String.format("File %s not found, its path: %s\n", filename, path));
+        }
+        return new World(read);
     }
 
 }
